9f7402da5543dd5055fde298f1555e7324ec3dd1
David Bouchare <dialindo7@gmail.com>
2016-08-26 14:59:34 +0200

    Fix indentation

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ db = redis.from_url(os.environ['REDISCLOUD_URL'])
 @app.route('/')
 def hello_world():
 	name = db.get('name') or 'World'
-    return 'Hello %s' % name
+	return 'Hello %s' % name
 
 @app.route('/setname/<name>')
 def setname(name):
-    db.set('name', name)
-    return 'Name updated'
+	db.set('name', name)
+	return 'Name updated'
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
