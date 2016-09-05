6adb0dce18c5d003c0bde4ff96558a06f0a37cc9
David Bouchare <dialindo7@gmail.com>
2016-09-05 21:33:53 +0200

    Add name, time and push retrieve to/from Redis

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import os
 import redis
-from flask import Flask
+import datetime
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 db = redis.from_url(os.environ['REDISCLOUD_URL'])
 
-@app.route('/')
-def hello_world():
-	name = db.get('name') or 'World'
-	return 'Hello %s' % name
 
-@app.route('/setname/<name>')
-def setname(name):
+@app.route('/')
+def index_form():
+    return render_template("index.html")
+
+@app.route('/setname', methods=['GET','POST'])
+def setname():
+	if request.method == 'GET':
+		return redirect(url_for('my_form'))
+	name = request.form['name'].lstrip()
+	time = datetime.datetime.now()
+	
 	db.set('name', name)
-	return 'Name updated'
+	db.set('time', time)
+
+	db_name = db.get('name')
+	db_time = db.get('time')
+
+	return render_template("setname.html", name=db_name, time=db_time)
+
 
 if __name__ == '__main__':
 	app.run()
+
