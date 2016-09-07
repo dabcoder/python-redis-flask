b82e9637cbae4332705aad50a259263f773c619a
dbouchare <dbouchare@gmail.com>
2016-09-07 23:19:54 +0200

    Add Openweathermap data

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 import redis
 import datetime
+import pyowm
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+#Redis
 db = redis.from_url(os.environ['REDISCLOUD_URL'])
-
+#Openweathermap API key
+owm = pyowm.OWM(os.environ['OWM_KEY']) 
 
 @app.route('/')
 def index_form():
@@ -15,19 +18,35 @@ def index_form():
 @app.route('/setname', methods=['GET','POST'])
 def setname():
 	if request.method == 'GET':
-		return redirect(url_for('my_form'))
+		return redirect(url_for('index_form'))
+
 	name = request.form['name'].lstrip()
-	time = datetime.datetime.now()
-	
+
+	forecast = owm.daily_forecast(name)
+	tomorrow = pyowm.timeutils.tomorrow()
+	ans = ''
+
+	if forecast.will_be_sunny_at(tomorrow) == True:
+		ans = 'Yes!'
+	else:
+		ans = 'No'
+
+	#Get temperatures
+	w = owm.weather_at_place(name).get_weather()
+	temp = w.get_temperature(unit='celsius')['db_temp']
+
+	#Set values in DB
 	db.set('name', name)
-	db.set('time', time)
+	db.set('sunny', ans)
+	db.set('temp', temp)
 
+	#Get values
 	db_name = db.get('name')
-	db_time = db.get('time')
+	db_sunny = db.get('sunny')
+	db_temp = db.get('temp')
 
-	return render_template("setname.html", name=db_name, time=db_time)
+	return render_template("setname.html", name=db_name, sunny=db_sunny, temp=db_temp)
 
 
 if __name__ == '__main__':
 	app.run()
-
